7f845964603b5875b8f46643546618b7087d28b1
CrazyBloo <CrazyBloo@users.noreply.github.com>
2022-06-28 20:13:03 -0400

    surface normals, init commit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+public class Program
+{
+    
+    // Image Setup
+    static float AspectRatio = 16.0f / 9.0f;
+    static int ImageWidth = 400;
+    static int ImageHeight = (int)(ImageWidth / AspectRatio);
+
+    //Viewport Setup
+    static float ViewportHeight = 2.0f;
+    static float ViewportWidth = AspectRatio * ViewportHeight;
+    static float FocalLength = 1.0f;
+    
+    //Viewport Transforms
+    static Vector3 Origin = new Vector3(0, 0, 0);
+    static Vector3 Horizontal = new Vector3(ViewportWidth, 0, 0);
+    static Vector3 Vertical = new Vector3(0, ViewportHeight, 0);
+    static Vector3 LowerLeftCorner = Origin - Horizontal / 2 - Vertical / 2 - new Vector3(0, 0, FocalLength);
+
+    public static string ImageBuffer = "P3\n" + ImageWidth + ' ' + ImageHeight + "\n255\n";
+    
+    static void Main()
+    {
+        //for every pixel in the image, calculate
+        for (int j = ImageHeight - 1; j >= 0; --j)
+        {
+            for (int i = 0; i < ImageWidth; ++i)
+            {
+                CalcColor(i, j);
+            }
+        }
+        
+        File.WriteAllText("image.ppm", ImageBuffer);
+    }
+
+    public static void CalcColor(float i, float j)
+    {
+        float u = (float)i / ((float)ImageWidth - 1f);
+        float v = (float)j / ((float)ImageHeight - 1f);
+
+        var ray = new Ray(Origin, LowerLeftCorner + u * Horizontal + v * Vertical - Origin);
+
+        Vector3 pixel_color = Ray.RayColor(ray);
+
+        WriteColor(pixel_color);
+
+        float percent = j / (float)ImageHeight;
+
+        Console.WriteLine($"Drawing new pixel {i}, {j}, {percent * 100}% remaining");
+
+    }
+
+
+    static void WriteColor(Vector3 color)
+    {
+        //convert the color from 0.0 to 1.0 to 0 to 255
+        int x = (int)(255 * color.X);
+        int y = (int)(255 * color.Y);
+        int z = (int)(255 * color.Z);
+
+        //append to end of string in ppm format
+        ImageBuffer = ImageBuffer + x + ' ' + y + ' ' + z + '\n';
+    }
+
+
+   
+
+    public static Vector3 UnitVector(Vector3 vec)
+    {
+        return vec / vec.Length();
+    }
+
+}
---
+++ Ray.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Ray
+{
+    public Vector3 Origin { get; set; }
+    public Vector3 Direction { get; set; }
+    
+    public Ray(Vector3 origin, Vector3 direction, float time = 0f)
+    {
+        Origin = origin;
+        Direction = direction;
+    }
+    
+    public Vector3 At(float t)
+    {
+        return Origin + t * Direction;
+    }
+
+    public static Vector3 RayColor(Ray ray)
+    {
+        var hit = HitSphere(new Vector3(0, 0, -1), 0.5f, ray);
+        if (hit > 0.0f)
+        {
+            Vector3 N = Program.UnitVector(ray.At(hit) - new Vector3(0, 0, -1));
+            return 0.5f * new Vector3(N.X + 1, N.Y + 1, N.Z + 1f);
+        }
+
+        var dir = Program.UnitVector(ray.Direction);
+
+
+        var t = 0.5f * (dir.Y + 1.0f);
+
+        var negt = 1.0f - t;
+
+        return new Vector3(1.0f * negt, 1.0f * negt, 1.0f * negt) + new Vector3(0.5f * t, 0.7f * t, 1.0f * t);
+
+    }
+
+    public static float HitSphere(Vector3 Center, float radius, Ray ray) {
+        
+        Vector3 oc = ray.Origin - Center;
+        
+        var a = ray.Direction.LengthSquared();
+        var halfb = Vector3.Dot(oc, ray.Direction);
+        var c = oc.LengthSquared() - radius * radius;
+        
+        var discriminant = halfb*halfb - a * c;
+
+        if (discriminant < 0)
+        {
+            return -1.0f;
+        }
+        else
+        {
+            return (-halfb - MathF.Sqrt(discriminant)) / a;
+        }
+
+    }
+
+}
